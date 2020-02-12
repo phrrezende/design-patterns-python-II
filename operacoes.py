2d9b4d908bf408e1dc5583116964481b2554c5cf
Paulo Humberto Rezende <paulo.rezende@ifsuldeminas.edu.br>
2020-02-12 13:52:30 -0300

    design pattern Visitor

--- operacoes.py
+++ operacoes.py
@@ -7,6 +7,19 @@ class Substracao(object):
 	def avalia(self):
 		return self.__expressao_esquerda.avalia() - self.__expressao_direita.avalia()
 
+	@property
+	def expressao_esquerda(self):
+		return self.__expressao_esquerda
+
+	@property
+	def expressao_direita(self):
+		return self.__expressao_direita
+
+	def aceita(self, visitor):
+		visitor.visita_subtracao(self)
+
+
+
 class Soma(object):
 
 	def __init__(self, expressao_esquerda, expressao_direita):
@@ -16,6 +29,17 @@ class Soma(object):
 	def avalia(self):
 		return self.__expressao_esquerda.avalia() + self.__expressao_direita.avalia()
 
+	@property
+	def expressao_esquerda(self):
+		return self.__expressao_esquerda
+
+	@property
+	def expressao_direita(self):
+		return self.__expressao_direita
+
+	def aceita(self, visitor):
+		visitor.visita_soma(self)
+
 class Numero(object):
 	def __init__(self, numero):
 		self.__numero = numero
@@ -23,8 +47,21 @@ class Numero(object):
 	def avalia(self):
 		return self.__numero
 
+	@property
+	def numero(self):
+		return self.__numero
+
+	def aceita(self, visitor):
+		visitor.visita_numero(self)
+	
+
 if __name__ == "__main__":
+
+	from impressao import Impressao
+
 	expressao_esquerda = Soma(Numero(10),Numero(20))
 	expressao_direita = Soma(Numero(5),Numero(2))
 	expressao_conta = Soma(expressao_esquerda,expressao_direita)
-	print(expressao_conta.avalia())
+
+	impressao = Impressao()
+	expressao_conta.aceita(impressao)
